152e57e2438e5c5342ed8ff95cb38dea20bcbbf0
marcus-andre <marcus-andre@hotmail.com>
2025-12-17 18:58:27 -0300

    fix Procfile code error

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wy@fd+xl1*3(hozi*n%eyz10=osl(=g2bb5))(ew&enr0r66h1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
 
 
 # Application definition
