d36a15421e2cf7ec70481f67cd01f218f536a704
marcus-andre <marcus-andre@hotmail.com>
2025-12-18 23:05:18 -0300

    Add meta class oredering and method __str__ for post

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,19 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comment_from_post")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comment_from_author")
+    body = models.TextField()
+    approved = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
