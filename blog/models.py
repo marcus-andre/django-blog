97248a458ece58cccdd7f96857a9cc86c0e56234
marcus-andre <marcus-andre@hotmail.com>
2025-12-18 23:14:02 -0300

    Add meta class oredering and method __str__ for comment model

--- blog/models.py
+++ blog/models.py
@@ -33,3 +33,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=True)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Comment {self.post} by {self.author}"
