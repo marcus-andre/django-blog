bb83afda12c2e9dd9b15282623a34da04816fff7
marcus-andre <marcus-andre@hotmail.com>
2025-12-20 20:46:41 -0300

    Add base template and homepage

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include("blog.urls"), name='index'),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path('', include("blog.urls"), name='index'),
 ]
